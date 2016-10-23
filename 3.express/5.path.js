fa92994f5b512e0fd744b2d581a7514df9080e2a
zhouhaiyangcls <1289226882@qq.com>
2016-10-23 11:30:28 +0800

    ''

--- 3.express/5.path.js
+++ 3.express/5.path.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-//在此中间件中判断用户是否登录
-// 如果请求的URL路径以/back开头的话，则执行此中间件，否则跳过不执行
+//在此中间录
+// 如果请求的URL路径以件中判断用户是否登/back开头的话，则执行此中间件，否则跳过不执行
 app.use('/front',function(req,res,next){
     //如果此用户已经登录
     if(true){
